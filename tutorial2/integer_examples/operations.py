dd9f4e28169ec86b48a60afce975148469803fc0
Cho Yiu Ng <choyiung@yahoo.com.hk>
2019-08-08 20:47:22 +0200

    Add example of 4 basic operations of integers in Python

---
+++ tutorial2/integer_examples/operations.py
@@ -0,0 +1,14 @@
+print("Input the value of x:")
+x = int(input())
+print("Input the value of y:")
+y = int(input())
+z = x + y
+print("x + y = {}".format(z))
+z = x - y
+print("x - y = {}".format(z))
+z = x * y
+print("x * y = {}".format(z))
+z = x / y
+print("x / y = {}".format(z))
+z = x // y
+print("x // y = {}".format(z))
