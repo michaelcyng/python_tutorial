5331a468b47116af78e4328461081f9e6b92efe8
Cho Yiu Ng (Michael) <choyiung@yahoo.com.hk>
2019-08-15 08:20:15 +0200

    An example for including comments in Python script (#15)
    * An example for including comments in Python file
    
    * Update gitignore to ignore the pycharm files

---
+++ tutorial3/comment_example/comment.py
@@ -0,0 +1,18 @@
+# Get the information from user
+print("Input the value of x:")
+x = float(input())
+print("Input the value of y:")
+y = float(input())
+
+# Perform operations on x and y
+z = x + y  # Addition
+print("x + y = {}".format(z))
+
+z = x - y  # Subtraction
+print("x - y = {}".format(z))
+
+z = x * y  # Multiplication
+print("x * y = {}".format(z))
+
+z = x / y  # Division
+print("x / y = {}".format(z))
