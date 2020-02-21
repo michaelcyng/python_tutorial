58b827dd662a061e6e1f7a17bf27c8907066813a
Cho Yiu Ng (Michael) <choyiung@yahoo.com.hk>
2020-02-21 23:20:32 +0100

    Add introductory example to tutorial 7 (#25)

---
+++ tutorial7/list_examples/introduction.py
@@ -0,0 +1,10 @@
+print("Input three kinds of fruits: ")
+fruit1 = input()
+fruit2 = input()
+fruit3 = input()
+print("Fruits inputted in reverse order: ")
+print(fruit3)
+print(fruit2)
+print(fruit1)
+
+# Question: What if the number of fruits to be entered is large (e.g. 100)?
