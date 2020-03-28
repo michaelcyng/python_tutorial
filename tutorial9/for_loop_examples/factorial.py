f01ce10ad04b2a367d7c0e905b57f16678651a5d
Cho Yiu Ng <choyiung@yahoo.com.hk>
2020-03-28 14:09:28 +0100

    Finished bug fix exercise in tutorial 9

--- tutorial9/for_loop_examples/factorial.py
+++ tutorial9/for_loop_examples/factorial.py
@@ -2,7 +2,7 @@ print("Input a non-negative integer: ")
 input_value = int(input())
 
 result = 1
-for number in range(input_value):
+for number in range(1, input_value + 1):
     result *= number
 
 print(result)
