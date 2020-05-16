fd379428bcfd71e528c53af6e3f9aabf37d48651
Cho Yiu Ng (Michael) <choyiung@yahoo.com.hk>
2020-05-16 11:13:35 +0200

    Add exercises for tutorial 10 (#32)

---
+++ tutorial10/exercises/question1.py
@@ -0,0 +1,21 @@
+# TODO: By using the utility package, draw the following triangle:
+#
+# Sample 1:
+# Input the size of the triangle: 3
+# *
+# **
+# ***
+# **
+# *
+#
+# Sample 2:
+# Input the size of the triangle: 5
+# *
+# **
+# ***
+# ****
+# *****
+# ****
+# ***
+# **
+# *
---
+++ tutorial10/exercises/question2.py
@@ -0,0 +1,7 @@
+# TODO: This program is to compute the length of the hypotenuse of a right-angled triangle, which is the side opposite
+#       to the right angle, after the user provides the lengths of the other two sides
+#
+# Sample 1:
+# Input the length of one side: 3
+# Input the length of the other side: 4
+# The length of the hypotenuse is 5.
