38ca5b90ecd4a434216a6a1b1454343851b3b48b
Cho Yiu Ng (Michael) <choyiung@yahoo.com.hk>
2020-11-19 23:39:26 +0100

    Further questions related to functions (#41)

---
+++ tutorial10/exercises/question3.py
@@ -0,0 +1,14 @@
+# TODO: Complete the following program by completing the function lcm so that the lowest common multiple (LCM) of two
+#       positive integers can be computed.
+
+# TODO: return the LCM of num1 and num2
+def lcm(num1, num2):
+    pass
+
+
+print("Input the first integer: ", end="")
+num1 = int(input())
+print("Input the second integer: ", end="")
+num2 = int(input())
+
+print(f"Their LCM is {lcm(num1, num2)}.")
---
+++ tutorial10/exercises/question4.py
@@ -0,0 +1,1 @@
+# TODO: Write a module which contains a function that counts the number of even numbers in a list
